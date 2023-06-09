4ebac2c6cbf835c8c78a3494c7bb36fd7fa3dddb
EddisonChen <eddisonchen1@gmail.com>
2023-06-09 14:05:13 -0400

    added auth0 in root

--- calorie-logger-frontend/src/index.js
+++ calorie-logger-frontend/src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import reportWebVitals from './reportWebVitals';v
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-rpmr77q8x3vx2n5u.us.auth0.com"
+    clientId="8mBTnGFt2I6pgPgxpS4mTGDISSSGSkcf"
+    authorizationParams={{redirect_uri: window.location.origin}}>
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
