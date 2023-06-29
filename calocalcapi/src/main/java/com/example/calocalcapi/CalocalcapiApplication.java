b1a5dee740ee42f7e0beb37007225817c343401a
EddisonChen <eddisonchen1@gmail.com>
2023-06-29 16:03:05 -0400

    created spring boot api

---
+++ calocalcapi/src/main/java/com/example/calocalcapi/CalocalcapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.calocalcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalocalcapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalocalcapiApplication.class, args);
+	}
+
+}
---
+++ calocalcapi/src/test/java/com/example/calocalcapi/CalocalcapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calocalcapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalocalcapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
