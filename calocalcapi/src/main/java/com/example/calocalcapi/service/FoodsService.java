cde4aa564584686ef5cc33e3053958790bc782c0
EddisonChen <eddisonchen1@gmail.com>
2023-06-30 13:27:41 -0400

    finished writing crud functions in service classes

--- calocalcapi/src/main/java/com/example/calocalcapi/service/FoodsService.java
+++ calocalcapi/src/main/java/com/example/calocalcapi/service/FoodsService.java
@@ -1,4 +1,49 @@
 package com.example.calocalcapi.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.calocalcapi.model.Foods;
+import com.example.calocalcapi.repository.FoodsRepository;
+import java.util.List;
+
+@Service
 public class FoodsService {
+
+    private FoodsRepository foodRepo;
+
+    @Autowired
+        public FoodsService(FoodsRepository foodRepo) {
+        this.foodRepo = foodRepo;
+    }
+
+    public Foods createFood(Foods food) {
+        return foodRepo.save(food);
+    }
+
+    public List<Foods> getFoods() {
+        return foodRepo.findAll();
+    }
+
+    public void deleteFood(Integer foodId) {
+        foodRepo.deleteById(foodId);
+    }
+
+    public Foods updateFood(Foods updatedFood) {
+        Foods existingFood = foodRepo.findById(updatedFood.getId()).orElse(null);
+
+        if (existingFood != null) {
+            existingFood.setDate(updatedFood.getDate());
+            existingFood.setMeal_type(updatedFood.getMeal_type());
+            existingFood.setName(updatedFood.getName());
+            existingFood.setAmount(updatedFood.getAmount());
+            existingFood.setCalories(updatedFood.getCalories());
+            existingFood.setProtein(updatedFood.getProtein());
+            existingFood.setCarbohydrate(updatedFood.getCarbohydrate());
+            existingFood.setFat(updatedFood.getFat());
+            return foodRepo.save(existingFood);
+        }
+
+        return existingFood;
+    }
 }
--- calocalcapi/src/main/java/com/example/calocalcapi/service/UsersService.java
+++ calocalcapi/src/main/java/com/example/calocalcapi/service/UsersService.java
@@ -1,4 +1,42 @@
 package com.example.calocalcapi.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.calocalcapi.model.Users;
+import com.example.calocalcapi.repository.UsersRepository;
+import java.util.List;
+
+@Service
 public class UsersService {
+
+    private UsersRepository userRepo;
+
+    @Autowired
+        public UsersService(UsersRepository userRepo) {
+            this.userRepo = userRepo;
+    };
+
+    public Users createUser(Users user) {
+        return userRepo.save(user);
+    }
+
+    public List<Users> getUsers() {
+        return userRepo.findAll();
+    }
+
+    public void deleteEmployee(String userId) {
+        userRepo.deleteById(userId);
+    }
+
+    public Users updateUser(Users updatedUser) {
+        Users existingUser = userRepo.findById(updatedUser.getId()).orElse(null);
+
+        if (existingUser != null) {
+            existingUser.setName(updatedUser.getName());
+            existingUser.setEmail(updatedUser.getEmail());
+            return userRepo.save(existingUser);
+        }
+        return existingUser;
+    }
 }
